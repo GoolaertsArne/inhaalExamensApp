e330db08b02c0dcf0ce0498ad0f2b664fd059c6b
GoolaertsArne <goolaerts.arne@hotmail.com>
2021-01-17 17:26:08 +0100

    student search added

--- routes/inhaalExamens.js
+++ routes/inhaalExamens.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient
 const { ObjectID } = require('mongodb');
 var db;
 
-MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true}, (err, database) => {
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (err, database) => {
   if (err) return console.log(err)
   db = database.db('examen')
 
@@ -23,66 +23,78 @@ MongoClient.connect('mongodb://localhost:27017', {useUnifiedTopology: true}, (er
 
   /* ADD PRODUCT TO DB */
   router.post('/add', (req, res) => {
-    db.collection('inhaal').insertOne(req.body, (err, result) => {
+    if (req.body.datum === '') {
       var today = new Date();
       var dd = String(today.getDate()).padStart(2, '0');
-      var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+      var mm = String(today.getMonth() + 1).padStart(2, '0');
       var yyyy = today.getFullYear();
       today = dd + '/' + mm + '/' + yyyy;
       req.body.datum = today;
-      console.log(req.body)
+    }
+    // console.log(req.body.datum)
+    req.body.key = req.body.student + req.body.examen + req.body.reden;
+    console.log(req.body.key)
+    db.collection('inhaal').find({key: req.body.key}).toArray((err, result) => {
       if (err) return
-      res.redirect('/')
+      if(!result[0]) {
+        db.collection('inhaal').insertOne(req.body, (err, result) => {
+          if (err) return
+          res.redirect('/')
+        })
+      }
+      else res.render('error', {message: 'Already exists'});
     })
   })
 
- // search form
-router.get('/search' , (req, res) =>{
-  res.render('search.ejs', {})
-})
+  // search form
+  router.get('/search', (req, res) => {
+    res.render('search.ejs', {})
+  })
 
-// find a product 
-// search_not_found.ejs werkt niet 
-router.post('/search', (req, res) => {
-var query = {student : req.body.student}
-db.collection('inhaal').findOne(query , (err, result) => {
-  if(err) return
-  if(result == '')
-    res.render('search_not_found.ejs', {})
-  else 
-    res.render('search_result.ejs' , {exaam : result})
-});
-})
+  // find a product 
+  // search_not_found.ejs werkt niet 
+  router.post('/search', (req, res) => {
+    var query = { student: req.body.student }
+    db.collection('inhaal').find(query).toArray((err, result) => {
+      if (err) return
+      if (result == '')
+        res.render('search_not_found.ejs', {})
+      else
+        console.log(result)
+        res.render('search_result.ejs', { examens: result })
+    });
+  })
 
-// delete a product
-router.post('/delete', (req, res) =>{
-  //var deletequery = {student : req.body.student}
-  var objectID = new ObjectID(req.body._id)
-  console.log(objectID)
-  db.collection('inhaal').findOneAndDelete( {_id : objectID } , (err, result)=> {
-    if(err) return res.send(500,err)
-      res.redirect('/')}
-  )
+  // delete a product
+  router.post('/delete', (req, res) => {
+    //var deletequery = {student : req.body.student}
+    var objectID = new ObjectID(req.body._id)
+    console.log(objectID)
+    db.collection('inhaal').findOneAndDelete({ _id: objectID }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.redirect('/')
+    }
+    )
   })
 
 
   //RENDER EDIT FORM 
-  router.get('/edit', (req,res) => {
+  router.get('/edit', (req, res) => {
     var query = { student: req.query.student }
-    db.collection('inhaal').findOne(query, (err,result) =>{
-      res.render('edit.ejs', {exaam: result})
+    db.collection('inhaal').findOne(query, (err, result) => {
+      res.render('edit.ejs', { exaam: result })
     })
   })
 
-  
-/* EDIT A PRODUCT */
-router.post('/edit', (req, res) => {
-  console.log({ student: req.body })
-  db.collection('inhaal').replaceOne({ student: req.body.student }, req.body) 
-  console.log(req.body)
-  res.redirect('/')
 
-})
+  /* EDIT A PRODUCT */
+  router.post('/edit', (req, res) => {
+    console.log({ student: req.body })
+    db.collection('inhaal').replaceOne({ student: req.body.student }, req.body)
+    console.log(req.body)
+    res.redirect('/')
+
+  })
 })
 
 
